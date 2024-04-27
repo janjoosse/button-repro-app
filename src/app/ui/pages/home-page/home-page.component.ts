635087802e92a10bf7fdff2b97c3a640930b1cc8
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-04-27 11:11:31 +0200

    Tryout

--- src/app/ui/pages/home-page/home-page.component.ts
+++ src/app/ui/pages/home-page/home-page.component.ts
@@ -1,14 +1,13 @@
-import { ChangeDetectionStrategy, Component, afterNextRender } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ContentService } from '../../../core/services/content.service';
 import { EventCardComponent } from '../../components/event-card/event-card.component';
 import { RichTextComponent } from '../../components/rich-text/rich-text.component';
-import { scrollLeftKey, scrollListKey } from '../../../app.config';
-import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [EventCardComponent, RichTextComponent, NgFor],
+  imports: [EventCardComponent, RichTextComponent, RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
